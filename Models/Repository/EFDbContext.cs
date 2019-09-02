e2d907ea5debbb5b30e2ad199f9746b1b781a5e2
Fiseyskiy Stanislav <fiseyskiysv@aquilon-st.ru>
2019-09-02 09:54:07 +0300

    load project

---
+++ Models/Repository/EFDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Data.Entity;
+
+namespace GameStore.Models.Repository
+{
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext()
+            : base(@"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename=" + HttpContext.Current.Server.MapPath("~/App_Data/GameStore.mdf")
+                    + ";Integrated Security=True")
+        {  }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # InetShopWebForms
 Online Store on ASP.NET Web Forms 4.5
+Simply example game store
